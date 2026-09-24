50f06edd682522fc6dba15772f49c9736580514d
Angela J <ajacobsen@republicservices.com>
2026-09-24 10:11:25 -0700

    Comment out unused import and adjust connection
    Comment out the import of get_active_session and update connection method.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -10,7 +10,7 @@ st.subheader(
   """**Choose the fruits you want in your custom Smoothie!**
   """
 )
-
+cnx = st.connection("snowflake")
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
