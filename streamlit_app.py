71dae55cd581b246dfaeea20c896bfd75484afaa
Angela J <ajacobsen@republicservices.com>
2026-09-25 15:00:46 -0700

    Parse JSON response from Smoothiefroot API

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,5 +49,6 @@ with st.form("smoothie_order", clear_on_submit=True):
    
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response).json()
+
 
