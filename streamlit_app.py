0e82027cda836d4842cd18a8cbaecb1619be6e7c
Angela J <ajacobsen@republicservices.com>
2026-09-25 15:44:17 -0700

    Clean up code by removing comments and whitespace
    Removed commented-out code and unnecessary whitespace.

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,33 +44,20 @@ with st.form("smoothie_order", clear_on_submit=True):
     )
 
     if ingredients_list:
-
         ingredients_string = ""
-
         for fruit_chosen in ingredients_list:
-
             ingredients_string += fruit_chosen + " "
-
             search_on = pd_df.loc[
                 pd_df["FRUIT_NAME"] == fruit_chosen,
-                "SEARCH_ON"
+                'SEARCH_ON'
             ].iloc[0]
-
-            st.write(
-                "The search value for",
-                fruit_chosen,
-                "is",
-                search_on
-            )
-
+#            st.write(   "The search value for",  fruit_chosen,  "is",  search_on )
             st.subheader(
                 fruit_chosen + " Nutrition Information"
             )
-
             smoothiefroot_response = requests.get(
                 f"https://my.smoothiefroot.com/api/fruit/"{search_on}"
             )
-
             st.dataframe(
                 data=smoothiefroot_response.json(),
                 use_container_width=True
