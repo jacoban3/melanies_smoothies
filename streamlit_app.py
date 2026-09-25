1b57cc063a1f2526935ce9c8ff679a834d9ccbfc
Angela J <ajacobsen@republicservices.com>
2026-09-25 15:19:15 -0700

    Add 'SEARCH_ON' column to dataframe selection
    Updated the dataframe selection to include 'SEARCH_ON' column.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ st.subheader(
 cnx = st.connection("snowflake")
 session = cnx.session()
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 with st.form("smoothie_order", clear_on_submit=True):
