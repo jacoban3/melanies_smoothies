ae1c5450171cbc9484e1485df14762f58153ba0f
Angela J <ajacobsen@republicservices.com>
2026-09-25 15:15:17 -0700

    Fix API request formatting in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ with st.form("smoothie_order", clear_on_submit=True):
             ingredients_string += fruit_chosen + ' '
             st.subheader(fruit_chosen + 'Nutrition Information')
             smoothiefroot_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+            "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
             )
 
             sf_df = st.dataframe(
