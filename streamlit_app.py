facb71aad6f42ee22e71b18284169d31ecf0cd09
Angela J <ajacobsen@republicservices.com>
2026-09-25 15:38:36 -0700

    Comment out st.stop() in streamlit_app.py
    Comment out the st.stop() line to prevent stopping the Streamlit app execution.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #Convert the snowpark Dataframe to a Pandas dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+#st.stop()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 with st.form("smoothie_order", clear_on_submit=True):
