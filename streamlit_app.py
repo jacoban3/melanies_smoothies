56e649f0421b1174b36cf690acceffe3c7c08d0e
Angela J <ajacobsen@republicservices.com>
2026-09-25 15:15:56 -0700

    Fix formatting of subheader in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ with st.form("smoothie_order", clear_on_submit=True):
 
         for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ' '
-            st.subheader(fruit_chosen + 'Nutrition Information')
+            st.subheader(fruit_chosen + ' Nutrition Information')
             smoothiefroot_response = requests.get(
             "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
             )
