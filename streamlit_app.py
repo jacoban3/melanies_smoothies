afa486ada4c06415de913dc515063e919866818e
Angela J <ajacobsen@republicservices.com>
2026-09-25 14:58:19 -0700

    Add API call to fetch watermelon data

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,7 @@ with st.form("smoothie_order", clear_on_submit=True):
         st.success('Your Smoothie is ordered!', icon="✅")
 
    
-
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 
