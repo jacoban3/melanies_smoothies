20b645561088c9da7d7d906165bf312739c4bcd3
Angela J <ajacobsen@republicservices.com>
2026-09-25 15:46:23 -0700

    Refactor subheader and API request formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,16 +52,9 @@ with st.form("smoothie_order", clear_on_submit=True):
                 'SEARCH_ON'
             ].iloc[0]
 #            st.write(   "The search value for",  fruit_chosen,  "is",  search_on )
-            st.subheader(
-                fruit_chosen + " Nutrition Information"
-            )
-            smoothiefroot_response = requests.get(
-                f"https://my.smoothiefroot.com/api/fruit/"{search_on}"
-            )
-            st.dataframe(
-                data=smoothiefroot_response.json(),
-                use_container_width=True
-            )
+            st.subheader(fruit_chosen + " Nutrition Information")            
+            smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/"{search_on}")
+            st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
        #st.write(ingredients_string) - inserts to snowflake table.
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
